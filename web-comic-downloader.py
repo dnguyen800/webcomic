afb80d332db1d7d2ac6091bad198c04acf1007f0
dnguyen800 <dnguyen800@hotmail.com>
2018-12-23 21:14:53 -0800

    data works

--- web-comic-downloader.py
+++ web-comic-downloader.py
@@ -8,54 +8,53 @@ import re                           # Regular expresssion object. Allows search 
 import requests                     # Sends HTTP request to website and saves the data
 from bs4 import BeautifulSoup       # Parses the HTML data into a Python object
 
+
+def checkURL(url):
+    """ Checks if URL is complete, then returns URL"""
+    print("url: ", url[0:4])
+    try:
+        if url[0:4] == 'http':                            # Check for full URL
+            print("Full URL provided.")
+            print(c['src'])
+            return c['src']
+        else:
+            full_url = url + c['src']      
+            print("Partial URL provided.")
+            print('full URL: %s' % full_url)
+            return full_url                          
+    except:
+        print("Error found. c['src'] does not exist")
+        return None
+                
+
 def pullWebComic(url, filename):
     # Get website data
     user_agent = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
     r = requests.get(url, headers=user_agent)
     soup = BeautifulSoup(r.text, 'html.parser')
-
+    image = None
     # Find any tags with id='comic'
-    comic = soup.find_all(id=re.compile("comic"))
-    print("Comic: \n", comic)
-    imgFound = False
     
-    while imgFound == False:     
+    
+    image_found = False
+    need_image = (image is None)         # Should default to true, need an image! Write a true statement, like variable is empty
+    
+    if need_image:
+        comic = soup.find_all(id=re.compile("comic"))
+        print("Comic: \n", comic)
         for c in comic:
-        # Check if comic <img> tag is found in first attempt           
-            try:               
-                print("c['src'] == ", c['src'])
-                if c['src'][0:4] == 'http':                            # Check for full URL
-                    print("Full URL provided.")
-                    print(c['src'])
-                    imgFound = True
-                else:                                                   # Assuming it is a partial URL
-                    full_url = url + c['src']      
-                    print("Partial URL provided.")
-                    print('full URL: %s' % full_url)
-                    imgFound = True
-            except:
-                print("Error found. c['src'] does not exist")
-                
-                # Continue looking for img tags in the child            
-            image = c.find_all('img')
-            
-                # if image is None, or there's nothing in the image object, search for another "comic" tag
-            try:
-                # image is found, so get the URL
-                print("image:\n", image)
-                for i in image:
-                    if i['src'][0:4] == 'http':                            # Check for full URL
-                        print("Full URL provided.")
-                        print(i['src'])
-                        imgFound = True
-                    else:                                                   # Assuming it is a partial URL
-                        full_url = url + i['src']      
-                        print("Partial URL provided.")
-                        print('full URL: %s' % full_url)
-                        imgFound = True                              
-            except:       
-                    # if img tag not found, then find the next div id = comic in child and start loop again
-                comic = comic.find_all(id=re.compile("comic"))    
+            print("c['src'] = ", c['src'])
+            image = checkURL(c['src'])           
+            if image is None:                  
+                img = c.find_all('img')
+                for i in img:
+                    print("i['src'] = ", i['src'])
+                    image = checkURL(i['src'])
+            else: break
+                # if img tag not found, then find the next div id = comic in child and start loop again
+            if image is None:
+                comic = comic.find_all(id=re.compile("comic"))
+            else: break
 
     
 #    
@@ -68,9 +67,9 @@ if __name__ == '__main__':
     lovenstein_url = 'http://www.mrlovenstein.com'
     pennyarcade_url = 'https://www.penny-arcade.com/comic'
 
-    pullWebComic(nedroid_url, 'nedroid.png')
-    pullWebComic(awkward_url, 'awkward.png')
-    pullWebComic(pennyarcade_url, 'pennyarcade.png')
+#    pullWebComic(nedroid_url, 'nedroid.png')
+#    pullWebComic(awkward_url, 'awkward.png')
+#    pullWebComic(pennyarcade_url, 'pennyarcade.png')
     pullWebComic(lovenstein_url, 'lovenstein.png')
 
-exit()
+    exit()
--- webcomic.py
+++ webcomic.py
@@ -8,12 +8,18 @@ from datetime import timedelta
 import logging
 import voluptuous as vol
 
+
+
 from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 
-REQUIREMENTS = ['requests', 'bs4']         # Python libraries or modules needed for this sensor
+REQUIREMENTS = ['bs4']         # Python libraries or modules needed for this sensor
 # DEPENDENCIES = ['mqtt']   # Other Home Assistant Components needed for this sensor to work.
+
+import re                           # Regular expresssion object. Allows search using wildcards
+import requests                     # Sends HTTP request to website and saves the data
+from bs4 import BeautifulSoup       # Parses the HTML data into a Python object
 
 __version__ = '0.0.1'
 _LOGGER = logging.getLogger(__name__)
@@ -27,7 +33,7 @@ CONF_REFRESH = 'refresh'
 DEFAULT_REFRESH = 360
 
 
-SCAN_INTERVAL = timedelta(minutes=360)
+SCAN_INTERVAL = timedelta(hours=1)
 
 # Validate the sensor's user-defined configuration specified in configuration.yaml
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
@@ -45,14 +51,15 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
 class ComicSensor(Entity):
     """Representation of a Sensor."""
-
     def __init__(self, hass, config):
         """Initialize the sensor and variables."""
-        self._state = None
+        
+        self.hass = hass
         self._name = config[CONF_NAME]
         self._url = config[CONF_URL]
+        self._state = None
         # This is where the comic URL data will be stored.
-        self.hass.data[self._name] = {}
+        self.hass.data[self._name] = ''
         self.update()
 
     @property
@@ -71,4 +78,46 @@ class ComicSensor(Entity):
 
         This is the only method that should fetch new data for Home Assistant.
         """
-        self._state = 23
+
+        self._state = 'Updating'
+        user_agent = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+        r = requests.get(self._url, headers=user_agent)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        comic = soup.find_all(id=re.compile("comic"))
+        self._state = "Beautiful Soup completed"
+        for c in comic:
+            try:
+                _LOGGER.info("c['src'] == %s ", c['src']) 
+                if c['src'][0:4] == 'http':                            
+                    _LOGGER.info("Full URL provided.")
+                    self.hass.data[self._name] = c['src']
+                    self._state = c['src']
+                    break
+                else:
+                    self.hass.data[self._name] = self._url + c['src']    
+                    _LOGGER.info("Partial URL provided. %s", (self._url + c['src']))
+                    self._state = self._url + c['src']
+                    break
+            except:
+                _LOGGER.info("c['src'] does not exist")
+                self._state = "Not found"
+            image = c.find_all('img')
+                
+            try:
+                _LOGGER.info("image: ")
+                for i in image:
+                    if i['src'][0:4] == 'http':                            
+                        _LOGGER.info("Full URL provided.")
+                        self.hass.data[self._name] = i['src']
+                        self._state = i['src']
+                        break
+                            
+                    else:
+                        self.hass.data[self._name] = self._url + i['src']     
+                        _LOGGER.info("Partial URL provided.")
+                        self._state = self._url + i['src'] 
+                        break                           
+            except:       
+                comic = comic.find_all(id=re.compile("comic"))
+                _LOGGER.info("nothing found. Searching for comic tag in child.")
+                self._state = "Not found"
